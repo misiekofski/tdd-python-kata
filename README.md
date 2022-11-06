e91acca098b7376b212325fa72bb55e17fb4a149
Michal Dobrzycki <dobrzycki@gmail.com>
2022-11-06 13:14:00 +0100

    Readme and some initial exercises

---
+++ README.md
@@ -0,0 +1,7 @@
+This is demo repo for Python unit test training
+
+
+
+```bash
+pytest --cov
+```
---
+++ palindrome/test_palindorome_with_params.py
@@ -0,0 +1,22 @@
+import pytest
+
+from palindrome import is_palindrome
+
+
+@pytest.mark.parametrize("palindrome", [
+    "",
+    "a",
+    "Bob",
+    "Never odd or even",
+    "Do geese see God?",
+])
+def test_is_palindrome(palindrome):
+    assert is_palindrome(palindrome)
+
+
+@pytest.mark.parametrize("non_palindrome", [
+    "abc",
+    "abab",
+])
+def test_is_palindrome_not_palindrome(non_palindrome):
+    assert not is_palindrome(non_palindrome)
